98a81249a83c62082a1767f5cabf56df89657e77
diwataoverloadpares <johncarlo.fernandez@sdca.edu.ph>
2024-10-12 16:14:04 +0800

    Add files via upload

---
+++ Fernandez/Ecommerse/authregister.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$fullName = $_POST["fullName"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$confirmpassword = $_POST["confirmpassword"];
+
+
+echo $fullName;
+
+
+
+
+
+
+?>
